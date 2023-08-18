63ea0237261785226bc46c5327c04d083742f612
r3aper89 <56981798+r3aper89@users.noreply.github.com>
2023-08-18 10:35:27 +0930

    feat: copy over previous CRUD RestAPI Extension Software Engineering Challenge

---
+++ ToDo.md
@@ -0,0 +1,9 @@
+# ToDo
+
+## JLO:
+
+- [x] copy over previous CRUD RestAPI Extension Software Engineering Challenge
+- [ ] fix path names
+- [ ] add swagger to Gradle - Groovy
+- [ ] add postgress sql
+- [ ] add devcontainer by git submodule
---
+++ restAPIchallge2/src/test/java/com/softwareengineeringchallenge/restAPIchallge2/RestApIchallge2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.softwareengineeringchallenge.restAPIchallge2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApIchallge2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
